7dcc69dbb3c52fa5f1b2e246ce8a51431a65fd67
adithyan232003 <145443649+adithyan232003@users.noreply.github.com>
2023-09-19 15:10:00 +0530

    my first comment

---
+++ Hellojava.java
@@ -0,0 +1,17 @@
+/*
+* File      :   HelloJava.java
+* description : My First Java Program the 
+                 "Hello Java"
+*Author      : sarju S
+*VERSION     : 1.0
+*DATE        : 18/09/2023
+* ********************/
+                 
+package javalab;
+
+public class Hellojava {
+	public static void main(String [] args) {
+	   System.out.println("Hello Java"); 
+	   System.out.print("false");
+}
+}
